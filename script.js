19e43c318ad70396356f7d8ff6630c6bb4708538
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-10-03 12:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,5 +9,28 @@ function decimalToBinary(num) {
     return "0";
   
 }
+	if (isNaN(num) || num < 0 || !Number.isInteger(num)) {
+    return "Invalid input. Please enter a positive integer.";
+  }
+
+  // Handle the special case of 0 separately
+  if (num === 0) {
+    return "0";
+  }
+
+  // Initialize an empty string to store the binary representation
+  let binary = "";
+
+  // Loop to convert the decimal number to binary
+  while (num > 0) {
+    // Append the least significant bit (LSB) of the number to the binary string
+    binary = (num % 2) + binary;
+    // Divide the number by 2 to shift to the next bit
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
+}
+
 
 window.decimalToBinary = decimalToBinary;
