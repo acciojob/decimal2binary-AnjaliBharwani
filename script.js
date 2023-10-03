df01f04eabd4213305bccc457ad518c1e9181959
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-10-03 12:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
+	if (isNaN(num) || num < 0 || !Number.isInteger(num)) {
+    return "Invalid input. Please enter a positive integer.";
+  }
+
+  // Handle the special case of 0 separately
+  if (num === 0) {
+    return "0";
   
 }
 
